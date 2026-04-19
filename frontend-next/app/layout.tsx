a0118e2a9a1d278a755e613705e5e65a8cc6a4d0
Taitil Chheda <Taitilchheda@users.noreply.github.com>
2026-04-19 20:55:58 +0530

    Update site and header logo to provided brain image (PNG) and set as favicon

--- frontend-next/app/layout.tsx
+++ frontend-next/app/layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" href="/brain-logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
